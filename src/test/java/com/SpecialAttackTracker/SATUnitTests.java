9d6cf4eb6069bd40e6e708a5e7a1052bd6309cb3
Godofdrakes <GodofDrakes@gmail.com>
2025-07-30 11:26:13 -0700

    Unit test file

--- src/test/java/com/SpecialAttackTracker/SATUnitTests.java
+++ src/test/java/com/SpecialAttackTracker/SATUnitTests.java
@@ -1,8 +1,10 @@
-﻿package com.SpecialAttackTracker;
+package com.SpecialAttackTracker;
 
 import junit.framework.TestCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SATUnitTests extends TestCase
 {
-    
+    private final Logger logger = LoggerFactory.getLogger( SATUnitTests.class );
 }
